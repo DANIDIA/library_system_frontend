4021830cc20bf1b9693b3e8a706f593095422710
DANIDIA <danik.serduk@gmail.com>
2024-05-07 08:22:18 +0200

    refactor(App): replace component to separate file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React from 'react';
+import LoginPage from './loginPage/LoginPage';
 
-function App() {
+export function App() {
     return (
-        <div>
-            <form>
-                <label>Login: </label><input type='text' name='login' />
-                <label>Password: </label><input type='password' name='password'/>
-            </form>
-            <a href='./librarian/Librarian.js'>Login as librarian</a>
-            <a href='./manager/Manager.js'>Login as manager</a>
-            <a href='./administrator/Administrator.js'>Login as admin</a>
-        </div>
+        <LoginPage/>
     );
 }
 
