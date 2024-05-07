4372fc3ea9999d4d2b56a2514a201d37d8550659
DANIDIA <danik.serduk@gmail.com>
2024-05-07 14:22:28 +0200

    feat(LoginPage): add login button login and login status text

--- src/loginPage/LoginPage.js
+++ src/loginPage/LoginPage.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {useNavigate} from 'react-router-dom';
 
+//TODO: replace to a separate file
 async function newSession(login, password){
+    //TODO: replace to constants
     const SERVER_PATH = 'http://localhost:5000';
     const endPoint = SERVER_PATH + '/sessions/login';
 
@@ -25,16 +28,40 @@ async function newSession(login, password){
 }
 
 export function LoginPage() {
+    const navigate = useNavigate();
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
+    const [authErrorText, setAuthErrorText] = useState('');
+
+    const handleLoginButtonClick = async () => {
+        const result = await newSession(login, password);
+
+        if (result.status === 401){
+            return setAuthErrorText('Login or password is incorrect');
+        }else if (result.status === 403){
+            return setAuthErrorText('User is blocked');
+        }
+
+        setAuthErrorText('');
+
+        // TODO: get rid of magic values
+        if(result.userRole === 1){
+            navigate('/administrator-panel/');
+        }else if(result.userRole === 2){
+            navigate('/department-manager-panel/');
+        }else if(result.userRole === 3){
+            navigate('/librarian-panel');
+        }
+    };
 
     return (
         <div>
-            <form>
-                <label>Login: </label><input type='text' name='login' />
-                <label>Password: </label><input type='password' name='password'/>
+            <form onSubmit={(e) => e.preventDefault()}>
+                <label>Login: </label><input value={login} onChange={e => setLogin(e.target.value)} type='text' name='login' />
+                <label>Password: </label><input value={password} onChange={e => setPassword(e.target.value)} type='password' name='password'/>
+                <button onClick={handleLoginButtonClick}>Login</button>
             </form>
-            <a href='../librarian/Librarian.js'>Login as librarian</a>
-            <a href='../manager/Manager.js'>Login as manager</a>
-            <a href='../administrator/Administrator.js'>Login as admin</a>
+            {authErrorText}
         </div>
     );
 }
