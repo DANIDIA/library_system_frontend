02ed28e9e071f37fd4970faf494ccdb2fcd7cf09
DANIDIA <danik.serduk@gmail.com>
2024-05-08 11:04:19 +0200

    refactor(LoginPage): improve code in handleLoginButtonClick

--- src/loginPage/LoginPage.js
+++ src/loginPage/LoginPage.js
@@ -12,23 +12,21 @@ export function LoginPage() {
         const result = await newSession(login, password);
 
         if (!result){
-            return setAuthErrorText('Oops... Some problems with internet connection or server. Please, check your connection');
-        }
-
-        if (result.status === 401){
-            return setAuthErrorText('Login or password is incorrect');
-        }else if (result.status === 403){
-            return setAuthErrorText('User is blocked');
+            setAuthErrorText('Oops... Some problems with internet connection or server. Please, check your connection');
+        } else if (result.status === 401){
+            setAuthErrorText('Login or password is incorrect');
+        } else if (result.status === 403){
+            setAuthErrorText('User is blocked');
         }
 
         setAuthErrorText('');
 
         // TODO: get rid of magic values
-        if(result.userRole === 1){
+        if(result?.userRole === 1){
             navigate('/administrator-panel/');
-        }else if(result.userRole === 2){
+        }else if(result?.userRole === 2){
             navigate('/department-manager-panel/');
-        }else if(result.userRole === 3){
+        }else if(result?.userRole === 3){
             navigate('/librarian-panel');
         }
     };
