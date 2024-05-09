bb16b8752e798374571e7408d61b033c5989770a
DANIDIA <danik.serduk@gmail.com>
2024-05-09 15:23:01 +0200

    refactor(booksServerOperations): wrap all function in object

--- src/helpers/booksServerOperations.js
+++ src/helpers/booksServerOperations.js
@@ -2,51 +2,69 @@ import { bases } from '../enums';
 import { create, deleteRecord, getData, update } from './CRUDOperations';
 import { fetchFromBase } from './fetchFromBase';
 
-export async function createBook(title, author, amount) {
-    await create(bases.BOOKS, { title, author, amount });
-}
-
-/**
- * @param params {{ title?:string, author?:string, amount?:number }}
- * @param fromRecordID {number} -1 means that start from the least id
- * @param recordAmount {number} -1 means maximum
- * @return {Promise<{
- * id:number,
- * title:string
- * author:string
- * amount:number} |
- * boolean |
- * {
- *  staus:number
- *  message:string}>}
- * */
-export async function getBooks(params, fromRecordID = -1, recordAmount = -1) {
-    return await getData(bases.BOOKS, fromRecordID, recordAmount, params);
-}
-
-/**
- * @param id {number}
- * @param params {{ title?:string, author:string, amount?:string }}
- * */
-export async function updateBook(id, params) {
-    await update(bases.BOOKS, id, params);
-}
-
-export async function deleteBook(id) {
-    await deleteRecord(bases.BOOKS, id);
-}
-
-export async function getGivenAmount(id) {
-    return await fetchFromBase(`${bases.BOOKS}/given_amount`, { id });
-}
-
-export async function giveToReader(bookID, readerID) {
-    return await fetchFromBase(
-        `${bases.BOOKS}/give_to_reader`,
-        {
-            bookID,
-            readerID,
+export function getBookServerOperations(sessionID) {
+    return {
+        createBook: async (title, author, amount) => {
+            return await create(
+                bases.BOOKS,
+                { title, author, amount },
+                sessionID,
+            );
         },
-        'post',
-    );
+
+        /**
+         * @param params {{ title?:string, author?:string, amount?:number }}
+         * @param fromRecordID {number} -1 means that start from the least id
+         * @param recordAmount {number} -1 means maximum
+         * @return {Promise<{
+         * id:number,
+         * title:string
+         * author:string
+         * amount:number} |
+         * boolean |
+         * {
+         *  staus:number
+         *  message:string}>}
+         * */
+        getBooks: async (params, fromRecordID = -1, recordAmount = -1) => {
+            return await getData(
+                bases.BOOKS,
+                fromRecordID,
+                recordAmount,
+                params,
+                sessionID,
+            );
+        },
+
+        /**
+         * @param id {number}
+         * @param params {{ title?:string, author:string, amount?:string }}
+         * */
+        updateBook: async (id, params) => {
+            return await update(bases.BOOKS, id, params, sessionID);
+        },
+
+        deleteBook: async (id) => {
+            return await deleteRecord(bases.BOOKS, id, sessionID);
+        },
+
+        getGivenAmount: async (id) => {
+            return await fetchFromBase(`${bases.BOOKS}/given_amount`, {
+                id,
+                sessionID,
+            });
+        },
+
+        giveToReader: async (bookID, readerID) => {
+            return await fetchFromBase(
+                `${bases.BOOKS}/give_to_reader`,
+                {
+                    bookID,
+                    readerID,
+                    sessionID,
+                },
+                'post',
+            );
+        },
+    };
 }
