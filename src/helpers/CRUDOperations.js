0a306e61bb247611c3b7e06bd62287276d2ff7af
DANIDIA <danik.serduk@gmail.com>
2024-05-09 15:22:23 +0200

    fix(CRUDOperations): add sessionID parameter to all functions

--- src/helpers/CRUDOperations.js
+++ src/helpers/CRUDOperations.js
@@ -1,22 +1,32 @@
 import { fetchFromBase } from './fetchFromBase';
 
-export async function create(base, data) {
-    return await fetchFromBase(`${base}/add`, data, 'post');
+export async function create(base, data, sessionID) {
+    return await fetchFromBase(`${base}/add`, { ...data, sessionID }, 'post');
 }
 
-export async function getData(base, fromRecordID, recordAmount, params) {
+export async function getData(
+    base,
+    fromRecordID,
+    recordAmount,
+    params,
+    sessionID,
+) {
     fromRecordID = fromRecordID > 0 ? fromRecordID : undefined;
     recordAmount = recordAmount > 0 ? recordAmount : undefined;
 
-    const query = { ...params, fromRecordID, recordAmount };
+    const query = { ...params, sessionID, fromRecordID, recordAmount };
 
     return fetchFromBase(`${base}/get`, query);
 }
 
-export async function update(base, id, params) {
-    return await fetchFromBase(`${base}/update`, { id, ...params }, 'put');
+export async function update(base, id, params, sessionID) {
+    return await fetchFromBase(
+        `${base}/update`,
+        { id, ...params, sessionID },
+        'put',
+    );
 }
 
-export async function deleteRecord(base, id) {
-    return await fetchFromBase(`${base}/remove`, { id }, 'delete');
+export async function deleteRecord(base, id, sessionID) {
+    return await fetchFromBase(`${base}/remove`, { id, sessionID }, 'delete');
 }
