e63629a730324d0fdfb8954c444bcfe0bb28b758
DANIDIA <danik.serduk@gmail.com>
2024-05-15 12:04:52 +0200

    feat(Router): add AuthLayout in router

--- src/router/Router.js
+++ src/router/Router.js
@@ -4,12 +4,15 @@ import {
     createBrowserRouter,
     createRoutesFromElements,
 } from 'react-router-dom';
+import { AuthLayout } from '../layouts';
 import { LoginView } from '../views';
 
 export const Router = createBrowserRouter(
     createRoutesFromElements(
         <Route path='/'>
-            <Route path='/' element={<LoginView />} />
+            <Route path='/' element={<AuthLayout />}>
+                <Route path='login' element={<LoginView />} />
+            </Route>
         </Route>,
     ),
 );
