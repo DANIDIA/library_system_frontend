a7743d17563a869717ef99da765087d73fd8978d
DANIDIA <danik.serduk@gmail.com>
2024-05-16 19:41:50 +0200

    feat(PanelLayout): add logout button

--- src/layouts/PanelLayout/PanelLayout.js
+++ src/layouts/PanelLayout/PanelLayout.js
@@ -2,12 +2,13 @@ import React, { useContext } from 'react';
 import { useNavigate, useOutlet } from 'react-router-dom';
 import { SessionContext } from '../../contexts';
 import { roles } from '../../shared';
+import { authPath } from '../AuthLayout';
 import { panelPaths } from './shared';
 
 export function PanelLayout() {
     const outlet = useOutlet();
     const navigate = useNavigate();
-    const userData = useContext(SessionContext);
+    const { userData, setUserData } = useContext(SessionContext);
 
     const adminLinks = [
         panelPaths.DEPARTMENTS_BASE,
@@ -52,8 +53,16 @@ export function PanelLayout() {
             break;
     }
 
+    function logout() {
+        navigate(authPath.LOGIN);
+        setUserData();
+    }
+
     return (
         <div>
+            <div>
+                <button onClick={logout}>logout</button>
+            </div>
             <nav>{buttons}</nav>
             {outlet}
         </div>
