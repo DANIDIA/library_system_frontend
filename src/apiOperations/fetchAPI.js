b840331ffb6d81da524fa2956f7260afa3a0eda7
DANIDIA <danik.serduk@gmail.com>
2024-05-23 17:49:38 +0200

    refactor(fetchAPI): rename variable

--- src/apiOperations/fetchAPI.js
+++ src/apiOperations/fetchAPI.js
@@ -1,17 +1,17 @@
 import { SERVER_PATH } from '../shared';
 
 export async function fetchAPI(sessionID, method, endpointPath, data) {
-    let path = `${SERVER_PATH}/${endpointPath}`;
+    let url = `${SERVER_PATH}/${endpointPath}`;
     const config = { method };
 
     if (method === 'get') {
-        path += '?' + new URLSearchParams({ ...data, sessionID }).toString();
+        url += '?' + new URLSearchParams({ ...data, sessionID }).toString();
     } else {
         config.body = JSON.stringify({ ...data, sessionID });
         config.headers = { 'Content-Type': 'application/json' };
     }
 
-    const response = await fetch(path, config).catch(() => {});
+    const response = await fetch(url, config).catch(() => {});
 
     if (!response?.ok) {
         return {
