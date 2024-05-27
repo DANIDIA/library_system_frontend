b1b67030b09fab10283443cb10cb454f40db8240
DANIDIA <danik.serduk@gmail.com>
2024-05-27 21:12:06 +0200

    fix(sessionAPIOperations): fix path

--- src/apiOperations/sessionsAPIOperations.js
+++ src/apiOperations/sessionsAPIOperations.js
@@ -1,5 +1,5 @@
 import { fetchAPI } from './fetchAPI';
 
 export async function newSession(login, password) {
-    return fetchAPI(null, 'post', '/sessions/login', { login, password });
+    return fetchAPI(null, 'post', 'sessions/login', { login, password });
 }
