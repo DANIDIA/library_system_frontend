1a59ed30eeffbd783e6929f392734b82c5e2f068
DANIDIA <danik.serduk@gmail.com>
2024-06-19 11:46:38 +0200

    chore(getDepartmentStatusMessage): change message text for user

--- src/views/PanelViews/DepartmentViews/helpers/getDepartmentStatusMessage.js
+++ src/views/PanelViews/DepartmentViews/helpers/getDepartmentStatusMessage.js
@@ -1,7 +1,7 @@
 import { getStatusMessageFunction } from '../../../../helpers';
 
 export const getDepartmentStatusMessage = getStatusMessageFunction({
-    400: 'There are workers or books in department',
+    400: 'There are no workers or books in department with requested params',
     403: 'Your session is ended, please login again',
     404: 'Some referencing data was invalid',
 });
