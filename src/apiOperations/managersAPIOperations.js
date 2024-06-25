3ef6a09e7a1f502d32fc7945e324103f5e042d74
DANIDIA <danik.serduk@gmail.com>
2024-06-25 14:27:51 +0200

    feat: add API for manipulating with users

---
+++ src/apiOperations/managersAPIOperations.js
@@ -0,0 +1,28 @@
+import { getCrudOperations } from './crudOperations';
+import { resources } from './shared';
+
+const operations = getCrudOperations(resources.EMPLOYEES);
+
+export async function createUser(data) {
+    return await operations.create(data);
+}
+
+export async function queryUsers(
+    queryParams,
+    pageSize = null,
+    pageNumber = null,
+) {
+    return await operations.query(queryParams, pageSize, pageNumber);
+}
+
+export async function getUser(id) {
+    return await operations.getData(id);
+}
+
+export async function updateUser(id, data) {
+    return await operations.update(id, data);
+}
+
+export async function deleteUser(id) {
+    return await operations.delete(id);
+}
