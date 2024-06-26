2861b83116ee0422bc640ffe118fe31572c5cd5f
DANIDIA <danik.serduk@gmail.com>
2024-06-26 14:50:12 +0200

    chore: add files to GIT

---
+++ src/views/PanelViews/ManagerViews/components/ManagerFormComponent/shared/consts.js
@@ -0,0 +1,10 @@
+export const managerFormFields = Object.freeze({
+    NAME: 'name',
+    SURNAME: 'surname',
+    PHONE_NUMBER: 'phoneNumber',
+    EMAIL: 'email',
+    LOGIN: 'login',
+    PASSWORD: 'password',
+    DEPARTMENT_DATA: 'departmentData',
+    STATUS: 'status',
+});
