88d292ed15d05195a484ce64d4e80c4b504ec6f5
DANIDIA <danik.serduk@gmail.com>
2024-06-28 18:59:37 +0200

    feat: make edit-view works

--- src/views/PanelViews/ManagerViews/ManagerEditView/ManagerEditView.js
+++ src/views/PanelViews/ManagerViews/ManagerEditView/ManagerEditView.js
@@ -1,21 +1,100 @@
-import React, { useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useContext, useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { getDepartment } from '../../../../apiOperations';
+import {
+    getUser,
+    updateUser,
+} from '../../../../apiOperations/usersAPIOperations';
 import { SessionContext } from '../../../../contexts';
 import { roles } from '../../../../shared';
-import { ManagerFormComponent } from '../components';
+import { hasChangedValues, hasEmptyFields } from '../../helpers';
+import { pathsInPanel } from '../../shared';
+import { managerContext } from '../ManagerContext';
+import { ManagerFormComponent, managerFormModes } from '../components';
+import { getManagerStatusMessage } from '../helpers';
 
 export function ManagerEditView() {
     const navigate = useNavigate();
+    const { managerID } = useParams();
     const { userData } = useContext(SessionContext);
+    const { managerData, setManagerData } = useContext(managerContext);
+    const [statusMessage, setStatusMessage] = useState('');
+
+    useEffect(() => {
+        (async () => {
+            if (managerData) return;
+
+            const managerDataResponse = await getUser(managerID);
+
+            if (!managerDataResponse.ok) {
+                setStatusMessage(getManagerStatusMessage(managerDataResponse));
+            }
+
+            if (!managerDataResponse.data.departmentID) {
+                setManagerData({ ...managerData });
+                return;
+            }
+
+            const departmentData = await getDepartment(
+                managerDataResponse.data.departmentID,
+            );
+
+            setManagerData({
+                ...managerDataResponse.data,
+                departmentData: departmentData,
+            });
+        })();
+    }, []);
+
+    const validateFormData = (formData) => {
+        if (hasEmptyFields(formData)) {
+            setStatusMessage('There are empty fields!');
+            return false;
+        }
+        if (!hasChangedValues(managerData, formData)) {
+            setStatusMessage('Values was not change');
+            return false;
+        }
+
+        return true;
+    };
+
+    const handleUpdate = async (formData) => {
+        console.log(formData);
+        if (!validateFormData(formData)) return;
+
+        const requestData = {
+            ...formData,
+            departmentID: formData?.departmentData?.id,
+        };
+
+        delete requestData.departmentData;
+
+        const response = await updateUser(formData.id, requestData);
+
+        if (!response.ok) {
+            setStatusMessage(getManagerStatusMessage(response));
+        } else {
+            setManagerData(formData);
+            navigate(`../${pathsInPanel.PAGE}/${managerID}`);
+        }
+    };
 
     return (
         <div>
             {userData.role === roles.ADMIN ? (
-                <ManagerFormComponent submitButtonText='update' />
+                <ManagerFormComponent
+                    submitButtonText='update'
+                    formMode={managerFormModes.FULL}
+                    formSubmitHandler={handleUpdate}
+                    initialValues={managerData}
+                />
             ) : (
                 'You do not have permission'
             )}
             <button onClick={() => navigate('..')}>back</button>
+            <br />
+            {statusMessage}
         </div>
     );
 }
