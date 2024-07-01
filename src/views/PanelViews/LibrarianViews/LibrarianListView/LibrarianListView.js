91a370cc9ced58a9b8ff8f0262df3530542d845c
DANIDIA <danik.serduk@gmail.com>
2024-07-01 19:19:05 +0200

    feat(LibrarianListView): makes librarians search works

--- src/views/PanelViews/LibrarianViews/LibrarianListView/LibrarianListView.js
+++ src/views/PanelViews/LibrarianViews/LibrarianListView/LibrarianListView.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { queryUsers } from '../../../../apiOperations/usersAPIOperations';
 import { roles } from '../../../../shared';
 import {
     UserFormComponent,
@@ -6,25 +7,57 @@ import {
     userFormModes,
 } from '../../components';
 import { UsersTable } from '../../components/UsersTable';
+import { getWithoutEmptyFields } from '../../helpers';
+import { getLibrarianStatusMessage } from '../helpers';
 
 export function LibrarianListView() {
+    const [librariansList, setLibrariansList] = useState([]);
+    const [statusMessage, setStatusMessage] = useState('');
     const [searchByFlags, setSearchByFlags] = useState({
         searchByDepartment: true,
         searchByStatus: true,
     });
 
+    const handleSearch = async (formData) => {
+        const requestData = {
+            ...getWithoutEmptyFields(formData),
+            departmentID: formData.departmentData?.id || null,
+            role: roles.LIBRARIAN,
+        };
+
+        delete requestData.departmentData;
+
+        if (!searchByFlags.searchByStatus) delete requestData.status;
+        if (!searchByFlags.searchByDepartment) delete requestData.departmentID;
+
+        const response = await queryUsers(requestData);
+
+        if (!response.ok) {
+            setStatusMessage(getLibrarianStatusMessage(response));
+            return;
+        }
+
+        setLibrariansList(response.data.results);
+    };
+
     return (
         <div>
             <UserFormComponent
                 submitButtonText='search managers'
-                employeeRole={roles.DEPARTMENT_MANAGER}
+                employeeRole={roles.LIBRARIAN}
                 formMode={userFormModes.FULL}
+                formSubmitHandler={handleSearch}
             />
             <UserSearchByFlagsComponent
                 initialValues={searchByFlags}
                 onChange={setSearchByFlags}
             />
-            <UsersTable />
+            {statusMessage} <br />
+            {librariansList.length > 0 ? (
+                <UsersTable list={librariansList} />
+            ) : (
+                'no results'
+            )}
         </div>
     );
 }
