7e3e1f9ba3a7d03fbf802f9e13bc61ff63f1d541
DANIDIA <danik.serduk@gmail.com>
2024-07-02 18:58:25 +0200

    feat: save librarian data in context

--- src/views/PanelViews/LibrarianViews/LibrarianListView/LibrarianListView.js
+++ src/views/PanelViews/LibrarianViews/LibrarianListView/LibrarianListView.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { queryUsers } from '../../../../apiOperations/usersAPIOperations';
 import { roles } from '../../../../shared';
 import {
@@ -8,9 +8,11 @@ import {
 } from '../../components';
 import { UsersTable } from '../../components/UsersTable';
 import { getWithoutEmptyFields } from '../../helpers';
+import { LibrarianContext } from '../LibrarianContext';
 import { getLibrarianStatusMessage } from '../helpers';
 
 export function LibrarianListView() {
+    const { setLibrarianData } = useContext(LibrarianContext);
     const [librariansList, setLibrariansList] = useState([]);
     const [statusMessage, setStatusMessage] = useState('');
     const [searchByFlags, setSearchByFlags] = useState({
@@ -40,6 +42,10 @@ export function LibrarianListView() {
         setLibrariansList(response.data.results);
     };
 
+    const handleClickOnTableItem = (userData) => {
+        setLibrarianData(userData);
+    };
+
     return (
         <div>
             <UserFormComponent
@@ -54,7 +60,10 @@ export function LibrarianListView() {
             />
             {statusMessage} <br />
             {librariansList.length > 0 ? (
-                <UsersTable list={librariansList} />
+                <UsersTable
+                    list={librariansList}
+                    onItemClick={handleClickOnTableItem}
+                />
             ) : (
                 'no results'
             )}
