d0fc6b4f782fbd79674d30c1f464d2f067ed26fa
DANIDIA <danik.serduk@gmail.com>
2024-07-18 19:09:55 +0200

    fix(ReadersCreationView): make that all added readers have active accounts

--- src/views/PanelViews/ReadersViews/ReaderCreationView/ReadersCreationView.js
+++ src/views/PanelViews/ReadersViews/ReaderCreationView/ReadersCreationView.js
@@ -1,19 +1,18 @@
-import emailValidator from 'email-validator';
-import { parsePhoneNumber } from 'libphonenumber-js';
 import React, { useState } from 'react';
 import { createReader } from '../../../../apiOperations';
-import { areNecessaryFieldsEmpty } from '../../helpers';
+import { accountStatuses } from '../../shared';
 import { ReaderFormComponent, readerFormModes } from '../components';
 import { getReaderStatusMessage, strictFormValidator } from '../helper';
-import { readerNecessaryFields } from '../shared';
 
 export function ReadersCreationView() {
     const [statusMessage, setStatusMessage] = useState('');
 
     const creationHandler = async (formData, clearForm) => {
-        if (!strictFormValidator(formData, setStatusMessage)) return;
+        const requestValues = { ...formData, status: accountStatuses.ACTIVE };
 
-        const response = await createReader(formData);
+        if (!strictFormValidator(requestValues, setStatusMessage)) return;
+
+        const response = await createReader(requestValues);
 
         if (!response.ok) {
             setStatusMessage(getReaderStatusMessage(response));
