29c83adfceb63411b65ac9c5d5d2d4e058851bab
DANIDIA <danik.serduk@gmail.com>
2024-07-19 18:53:45 +0200

    feat: add ability to select department manager

--- src/views/PanelViews/DepartmentViews/components/DepartmentFormComponent/DepartmentFormComponent.js
+++ src/views/PanelViews/DepartmentViews/components/DepartmentFormComponent/DepartmentFormComponent.js
@@ -1,13 +1,20 @@
 import React, { useState } from 'react';
-import { InputField } from '../../../../../components';
-import { getEmptyFields } from '../../../helpers';
+import { useLocation } from 'react-router-dom';
+import { InputField, SelectResourceComponent } from '../../../../../components';
+import { getEmptyFields, saveFormData } from '../../../helpers';
 import { departmentFormFields } from './consts';
 
 export function DepartmentFormComponent({
     submitButtonText,
     formSubmitHandler = () => {},
-    initialValues = { name: '', address: '', contactNumber: '' },
+    initialValues = {
+        name: '',
+        address: '',
+        contactNumber: '',
+        managerData: null,
+    },
 }) {
+    const location = useLocation();
     const [formValues, setFormValues] = useState({ ...initialValues });
 
     const clearForm = () => {
@@ -39,11 +46,19 @@ export function DepartmentFormComponent({
                 />
                 <InputField
                     name='Contact number:'
-                    initailValu={formValues.address}
+                    initailValue={formValues.address}
                     onChange={handleOnChange(
                         departmentFormFields.CONTACT_NUMBER,
                     )}
                     type='tel'
+                />
+                <SelectResourceComponent
+                    fieldName='department manager:'
+                    initialValue={formValues.managerData}
+                    onChange={handleOnChange(departmentFormFields.MANAGER_DATA)}
+                    onRedirect={() =>
+                        saveFormData(location.pathname, formValues)
+                    }
                 />
             </div>
 
--- src/views/PanelViews/DepartmentViews/components/DepartmentFormComponent/consts.js
+++ src/views/PanelViews/DepartmentViews/components/DepartmentFormComponent/consts.js
@@ -2,4 +2,5 @@ export const departmentFormFields = Object.freeze({
     NAME: 'name',
     ADDRESS: 'address',
     CONTACT_NUMBER: 'contactNumber',
+    MANAGER_DATA: 'managerData',
 });
